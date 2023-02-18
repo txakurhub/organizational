43c9d70ba6c24e661427a4a05d35eeb1f90d845e
Leandro Pereyra <me.leandrop@gmail.com>
2023-02-18 18:43:41 -0300

    intl provider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,51 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import Header from '../partials/Header';
-import PageIllustration from '../partials/PageIllustration';
-import HeroHome from '../partials/HeroHome';
-import FeaturesBlocks from '../partials/FeaturesBlocks';
-import FeaturesZigZag from '../partials/FeaturesZigzag';
-import Testimonials from '../partials/Testimonials';
-import Newsletter from '../partials/Newsletter';
-import Banner from '../partials/Banner';
-import Footer from '../partials/Footer';
+import Header from "../partials/Header";
+import PageIllustration from "../partials/PageIllustration";
+import HeroHome from "../partials/HeroHome";
+import FeaturesBlocks from "../partials/FeaturesBlocks";
+import FeaturesZigZag from "../partials/FeaturesZigzag";
+import Testimonials from "../partials/Testimonials";
+import Newsletter from "../partials/Newsletter";
+import Banner from "../partials/Banner";
+import Footer from "../partials/Footer";
+import { IntlProvider } from "react-intl";
+import { allMessages } from "../messages";
 
 function Home() {
+  const [lang, setLang] = useState(true);
+  const currentLocale = lang ? "en" : "es";
+
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
-      {/*  Site header */}
-      <Header />
+    <IntlProvider locale="en" messages={allMessages[currentLocale]}>
+      <div className="flex flex-col min-h-screen overflow-hidden">
+        {/*  Site header */}
+        <Header lang={lang} setLang={setLang} />
 
-      {/*  Page content */}
-      <main className="grow">
-        {/*  Page illustration */}
-        <div className="relative max-w-6xl mx-auto h-0 pointer-events-none" aria-hidden="true">
-          <PageIllustration />
-        </div>
+        {/*  Page content */}
+        <main className="grow">
+          {/*  Page illustration */}
+          <div
+            className="relative max-w-6xl mx-auto h-0 pointer-events-none"
+            aria-hidden="true"
+          >
+            <PageIllustration />
+          </div>
 
-        {/*  Page sections */}
-        <HeroHome />
-        <FeaturesBlocks />
-        <FeaturesZigZag />
-        {/* <Testimonials /> */}
-        <Newsletter />
-      </main>
+          {/*  Page sections */}
+          <HeroHome />
+          <FeaturesBlocks />
+          <FeaturesZigZag />
+          {/* <Testimonials /> */}
+          <Newsletter />
+        </main>
 
-      <Banner />
+        <Banner />
 
-      {/*  Site footer */}
-      <Footer />
-    </div>
+        {/*  Site footer */}
+        <Footer />
+      </div>
+    </IntlProvider>
   );
 }
 
