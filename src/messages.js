66c8cff14c57a48065c7f1ec0ddc9093e5da4ebf
Leandro Pereyra <me.leandrop@gmail.com>
2023-02-18 18:42:41 -0300

    messages.js

---
+++ src/messages.js
@@ -0,0 +1,115 @@
+export const allMessages = {
+  es: {
+    // ---- headerEADER
+    "header.work": "Proceso",
+    "header.prod": "Productos",
+    "header.cont": "Contacto",
+    // ---- headerERO
+    "hero.title": "Desatemos esa idea y observemos la magia en acción.",
+    "hero.sub":
+      "Nos apasiona convertir tu visión en una presencia web funcional y visualmente asombrosa. Trabajemos juntos para crear una experiencia digital que deje una impresión duradera.",
+    "hero.primary": "Empecemos",
+    "hero.secondary": "Aprender más",
+    // -------- WORKFLOW
+    "wf.title":
+      "La mayoría de los clientes no comprenden sus procesos de trabajo.",
+    "wf.sub":
+      "No dejes que la falta de comprensión de tu flujo de trabajo te frene, deja que te ayudemos a agilizar tus procesos y aumentar tu eficiencia.",
+    "wf.plan": "Planificación",
+    "wf.planDesc":
+      "¡La planificación es la mejor estrategia! Con el plan correcto, puedes lograr cualquier cosa que te propongas. Es como jugar ajedrez, necesitas pensar en el futuro y anticipar los movimientos de tu oponente.",
+    "wf.design": "Diseño",
+    "wf.designDesc":
+      "Desde la ropa que vestimos hasta los edificios en los que vivimos, el diseño está presente en todo lo que nos rodea. Se trata de resolver problemas y crear soluciones que sean elegantes y eficientes.",
+    "wf.dev": "Desarrollo",
+    "wf.devDesc":
+      "El desarrollo es el proceso alquímico de transformar sueños en código. Es el arte de dar vida a las ideas, de materializar algo nuevo y emocionante.",
+    "wf.launch": "Lanzamiento",
+    "wf.launchDesc":
+      "El lanzamiento es el momento en el que todo tu esfuerzo da frutos. Es el momento en que das a conocer tu creación al mundo y la ves propagarse como la pólvora.",
+    "wf.pos": "Posicionamiento",
+    "wf.posDesc":
+      "La optimización efectiva para motores de búsqueda (SEO) puede aumentar el tráfico orgánico a un sitio web, mejorar su visibilidad en línea y, en última instancia, llevar a una mayor cantidad de conversiones y de ingresos.",
+    "wf.feed": "Feedback",
+    "wf.feedDesc":
+      "Sabemos lo fundamental que es crear un sitio web que satisfaga tus necesidades y conecte con tu audiencia, y no nos detendremos hasta conseguir ese propósito.",
+    //   -----------------PRODUCT
+    "prod.lema": "Alcanza objetivos que importen.",
+    "prod.title": "Un producto, infinitas soluciones",
+    "prod.subtitle":
+      "Una muestra de mis últimos proyectos y colaboraciones, que abarcan diferentes industrias y plataformas, desde el desarrollo web hasta aplicaciones móviles y más.",
+    "prod.itSolutions": "Soluciones de IT y Consultoría",
+    "prod.ad": "Marketing y publicidad",
+    "prod.mobile": "Bienes Raíces",
+    // ---------------------FORM
+    "form.title": "No te pierdas nada",
+    "form.subtitle": "Empieza la charla",
+    "form.name": "Tu nombre",
+    "form.mail": "Email",
+    "form.msg": "Tu mensaje",
+    "form.submit": "Enviar!",
+    // ---------------FIXED
+    "fix.check": "Puedes revisar mi Github",
+    "fix.or": "o",
+    "fix.link": "mi LinkedIn",
+  },
+  en: {
+    // ---- hEADER
+    "header.work": "Workflow",
+    "header.prod": "Products",
+    "header.cont": "Contact",
+    // ---- hERO
+    "hero.title": "Let's unleash that idea and watch the magic happen.",
+    "hero.sub":
+      "We are passionate about transforming your vision into a functional, visually-stunning web presence. Let's work together to create a digital experience that leaves a lasting impression.",
+    "hero.primary": "Get Started",
+    "hero.secondary": "Learn More",
+    // -------- WORKFLOW
+    "wf.title": "The majority our customers do not understand their workflows.",
+    "wf.sub":
+      "Don't let a lack of understanding of your workflow hold you back, let us help you streamline your processes and increase your efficiency.",
+
+    "wf.plan": "Planification",
+    "wf.planDesc":
+      "Planning is the ultimate power move! With the right plan, you can achieve anything you set your mind to. It's like a game of chess, you need to think ahead and anticipate your opponent's moves.",
+
+    "wf.design": "Design",
+    "wf.designDesc":
+      "From the clothes we wear to the buildings we live in, design is all around us. It's about solving problems and creating solutions that are both elegant and efficient.",
+
+    "wf.dev": "Development",
+    "wf.devDesc":
+      "Development is the alchemical process of turning dreams into code. It is the art of breathing life into ideas, of bringing something new and exciting into existence.",
+
+    "wf.launch": "Launching",
+    "wf.launchDesc":
+      "Launching is the moment where all your hard work comes to fruition. It's the moment where you unleash your creation into the world and watch it spread like wildfire.",
+
+    "wf.pos": "Positioning",
+    "wf.posDesc":
+      "Effective SEO can increase organic traffic to a website, improve its online visibility, and ultimately lead to higher conversions and revenue.",
+
+    "wf.feed": "Feedback",
+    "wf.feedDesc":
+      "We understand the importance of creating a website that meets your needs and resonates with your audience, and we won't stop until we achieve that goal.",
+    //   -----------------PRODUCT
+    "prod.lema": "Reach goals that matter",
+    "prod.title": "One product, unlimited solutions",
+    "prod.subtitle":
+      "A showcase of my latest projects and collaborations, spanning a variety of industries and platforms, from web development to mobile apps and more",
+    "prod.itSolutions": "IT solutions and consulting",
+    "prod.ad": "Advertising services",
+    "prod.mobile": "Real State",
+    // ---------------------FORM
+    "form.title": "Stay in the loop",
+    "form.subtitle": "Get the conversation started",
+    "form.name": "Name",
+    "form.mail": "Email",
+    "form.msg": "Message",
+    "form.submit": "Send it!",
+    // ---------------FIXED
+    "fix.check": "Check my GitHub",
+    "fix.or": "or",
+    "fix.link": "my LinkedIn",
+  },
+};
