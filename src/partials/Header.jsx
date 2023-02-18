15ce6ad9fd405a7cb4bd651650d52cebd9e03b08
Leandro Pereyra <me.leandrop@gmail.com>
2023-02-18 18:44:11 -0300

    header fix

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Dropdown from "../utils/Dropdown";
 
-function Header() {
+function Header({ lang, setLang }) {
   const [mobileNavOpen, setMobileNavOpen] = useState(false);
 
   const trigger = useRef(null);
@@ -42,13 +42,44 @@ function Header() {
           <div className="shrink-0 mr-4">
             {/* Logo */}
             <Link to="/" className="block" aria-label="Cruip">
-              <svg
-                className="w-8 h-8 fill-current text-purple-600"
-                viewBox="0 0 32 32"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M31.952 14.751a260.51 260.51 0 00-4.359-4.407C23.932 6.734 20.16 3.182 16.171 0c1.634.017 3.21.28 4.692.751 3.487 3.114 6.846 6.398 10.163 9.737.493 1.346.811 2.776.926 4.262zm-1.388 7.883c-2.496-2.597-5.051-5.12-7.737-7.471-3.706-3.246-10.693-9.81-15.736-7.418-4.552 2.158-4.717 10.543-4.96 16.238A15.926 15.926 0 010 16C0 9.799 3.528 4.421 8.686 1.766c1.82.593 3.593 1.675 5.038 2.587 6.569 4.14 12.29 9.71 17.792 15.57-.237.94-.557 1.846-.952 2.711zm-4.505 5.81a56.161 56.161 0 00-1.007-.823c-2.574-2.054-6.087-4.805-9.394-4.044-3.022.695-4.264 4.267-4.97 7.52a15.945 15.945 0 01-3.665-1.85c.366-3.242.89-6.675 2.405-9.364 2.315-4.107 6.287-3.072 9.613-1.132 3.36 1.96 6.417 4.572 9.313 7.417a16.097 16.097 0 01-2.295 2.275z" />
-              </svg>
+              <div className="flex items-center hover:text-purple-600 ease-in duration-300">
+                {/* <svg
+                  className="w-8 h-8 fill-current text-purple-600"
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M31.952 14.751a260.51 260.51 0 00-4.359-4.407C23.932 6.734 20.16 3.182 16.171 0c1.634.017 3.21.28 4.692.751 3.487 3.114 6.846 6.398 10.163 9.737.493 1.346.811 2.776.926 4.262zm-1.388 7.883c-2.496-2.597-5.051-5.12-7.737-7.471-3.706-3.246-10.693-9.81-15.736-7.418-4.552 2.158-4.717 10.543-4.96 16.238A15.926 15.926 0 010 16C0 9.799 3.528 4.421 8.686 1.766c1.82.593 3.593 1.675 5.038 2.587 6.569 4.14 12.29 9.71 17.792 15.57-.237.94-.557 1.846-.952 2.711zm-4.505 5.81a56.161 56.161 0 00-1.007-.823c-2.574-2.054-6.087-4.805-9.394-4.044-3.022.695-4.264 4.267-4.97 7.52a15.945 15.945 0 01-3.665-1.85c.366-3.242.89-6.675 2.405-9.364 2.315-4.107 6.287-3.072 9.613-1.132 3.36 1.96 6.417 4.572 9.313 7.417a16.097 16.097 0 01-2.295 2.275z" />
+                </svg> */}
+
+                <svg
+                  className="w-8 h-8 fill-current text-purple-600 "
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <text
+                    x="45%"
+                    y="50%"
+                    text-anchor="middle"
+                    font-size="20"
+                    font-weight="bold"
+                    dominant-baseline="central"
+                  >
+                    L
+                  </text>
+                  <text
+                    x="75%"
+                    y="66%"
+                    text-anchor="middle"
+                    font-size="20"
+                    font-weight="bold"
+                    dominant-baseline="central"
+                  >
+                    P
+                  </text>
+                </svg>
+
+                <h2 className="mx-5">Leandro Pereyra</h2>
+              </div>
             </Link>
           </div>
 
@@ -80,6 +111,15 @@ function Header() {
                 >
                   Contact
                 </Link>
+              </li>
+
+              <li>
+                <button
+                  onClick={() => setLang(!lang)}
+                  className="text-gray-300 hover:text-gray-200 px-4 py-2 flex items-center transition duration-150 ease-in-out"
+                >
+                  ES
+                </button>
               </li>
 
               {/* 1st level: hover */}
@@ -144,9 +184,15 @@ function Header() {
                     Contact
                   </Link>
                 </li>
-               
-  
-        
+
+                <li>
+                  <button
+                    onClick={() => setLang(!lang)}
+                    className="flex text-gray-300 hover:text-gray-200 py-2"
+                  >
+                    ES
+                  </button>
+                </li>
               </ul>
             </nav>
           </div>
