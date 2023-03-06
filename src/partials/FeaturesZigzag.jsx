11edc0029aebadf0d389bdacffc2a6c4db08ccfa
Leandro Pereyra <me.leandrop@gmail.com>
2023-03-06 19:29:20 -0300

    sinfronteras feature

--- src/partials/FeaturesZigzag.jsx
+++ src/partials/FeaturesZigzag.jsx
@@ -21,37 +21,91 @@ function FeaturesZigzag() {
 
           {/* Items */}
           <div className="grid gap-20">
-            {/* SIN FRONTERAS IMG LEFT */}
-            {/* <div className="md:grid md:grid-cols-12 md:gap-6 items-center"> */}
-            {/* Image */}
-            {/* <div
+            {/* SIN FRONTERAS IMG LEFT  */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
                 className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl"
                 data-aos="fade-up"
               >
                 <img
                   className="max-w-full mx-auto md:max-w-none h-auto"
-                  src=""
+                  src="https://res.cloudinary.com/ddkurzft6/image/upload/v1677860396/sin%20fronteras/sfpresentation_zsuybi.png"
                   width="540"
                   height="405"
                   alt="Features 02"
                 />
-              </div> */}
-            {/* Content */}
-            {/* <div
+              </div>
+              {/* Content */}
+              <div
                 className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
                 data-aos="fade-left"
               >
                 <div className="md:pl-4 lg:pl-12 xl:pl-16">
                   <div className="font-architects-daughter text-xl text-purple-600 mb-2">
-                    <FormattedMessage id="prod.ad" />
+                    {/* <FormattedMessage id="prod.ad" /> */} Social Network App
                   </div>
                   <h3 className="h3 mb-3">Sin Fronteras App</h3>
-                  <p className="text-xl text-gray-400 mb-4">
-                    <FormattedMessage id="wf.gt.desc" />
-                  </p>
+                  <ul className="text-lg text-gray-400 -mb-2">
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>
+                        Designing and developing the application's user
+                        interface, following design standards and guidelines.
+                      </span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>
+                        Collaborating with the design team to translate designs
+                        into interactive components and functionalities using
+                        agile methodologies (Scrum).
+                      </span>
+                    </li>
+                    <li className="flex items-center">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>
+                        Implementing and maintaining navigation between
+                        different screens of the application, ensuring a smooth
+                        user experience.
+                      </span>
+                    </li>
+                    <li className="flex items-center">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>
+                        Integrating the application with backend services,
+                        following API specifications and ensuring correct
+                        functionality.
+                      </span>
+                    </li>
+                  </ul>
                 </div>
               </div>
-            </div> */}
+            </div>
 
             {/* SOFFYSOFT IMG RIGHT */}
             <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
