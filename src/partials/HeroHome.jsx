6ffe63f4998c917c181180584cd7318c5d9fe6fa
Leandro Pereyra <me.leandrop@gmail.com>
2023-03-30 17:20:16 -0300

    mt herotitle mobile

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -50,7 +50,7 @@ function HeroHome() {
         {/* Hero content */}
         <div className="relative pt-32 pb-10 md:pt-40 md:pb-16">
           {/* Section header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+          <div className="max-w-3xl mx-auto text-center mt-20 lg:mt-0 pb-12 md:pb-16">
             <h1 className="h1 mb-4" data-aos="fade-up">
               <FormattedMessage id="hero.title" />
             </h1>
