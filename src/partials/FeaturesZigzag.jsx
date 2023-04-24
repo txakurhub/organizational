0ea28c9269727e3c969f00e1d8adc628a137a718
Leandro Pereyra <me.leandrop@gmail.com>
2023-04-24 17:28:50 -0300

    pawsitiveI

--- src/partials/FeaturesZigzag.jsx
+++ src/partials/FeaturesZigzag.jsx
@@ -21,6 +21,76 @@ function FeaturesZigzag() {
 
           {/* Items */}
           <div className="grid gap-20">
+            {/* SOFFYSOFT IMG RIGHT */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                data-aos="fade-up"
+              >
+                <a href="https://github.com/No-Country/s7-27-m-desafioreactnative">
+                  <img
+                    className="max-w-full mx-auto md:max-w-none h-auto"
+                    src="https://res.cloudinary.com/ddkurzft6/image/upload/v1682120941/pawsitive/nop_h1v8vc.jpg"
+                    width="540"
+                    height="405"
+                    alt="Features 01"
+                  />
+                </a>
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-right"
+              >
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">
+                    <FormattedMessage id="prod.itSolutions" />
+                  </div>
+                  <h3 className="h3 mb-3">Pawsitive</h3>
+                  {/* <p className="text-xl text-gray-400 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> */}
+                  <ul className="text-lg text-gray-400 -mb-2">
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>
+                        <FormattedMessage id="wf.sf.1" />
+                      </span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>
+                        <FormattedMessage id="wf.sf.2" />
+                      </span>
+                    </li>
+                    <li className="flex items-center">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>
+                        <FormattedMessage id="wf.sf.3" />
+                      </span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
             {/* SIN FRONTERAS IMG LEFT  */}
             <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
               {/* Image */}
